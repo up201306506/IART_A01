7fc64edc33cc30f544c08b19b5601839f83b50f3
Tropical Drink <emailfalsonumero4@gmail.com>
2016-05-24 19:51:54 +0100

    Euclidian

--- Eclipse Project/src/logic/HeuristicsUtils.java
+++ Eclipse Project/src/logic/HeuristicsUtils.java
@@ -2,6 +2,38 @@ package logic;
 
 public class HeuristicsUtils {
 	
-	// implementation of aux heuristics methods
-	// only static methods please
+	// implementation of aux heuristics methods	
+
+	public static int UnrestrictedHeuristic(Node currentN, Node targetNode){
+		Double h = 0.0;
+		h += DistanceValue(currentN, targetN);
+		
+		
+		return h.intValue();
+	}
+	
+	public static double DistanceValue(Node A, Node B){
+		//Equals its Euclidean Distance
+		double dx = Math.abs(B.getXCord() - A.getXCord());
+		double dy = Math.abs(B.getYCord() - A.getYCord());
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+	
+	public static double CostValue(Node A, Node B){
+		return 0;	
+	}
+	
+	public static double DurationValue(Node A, Node B){
+		return 0;
+	}
+	
+	public static double DrowzinessValue(Node A, Node B){
+		return 0;
+	}
+	
+	public static double DrowzinessValue(Node A, Node B){
+		return 0;
+	}
+	
+	
 }
