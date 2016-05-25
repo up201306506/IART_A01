b5eacd4f3e946a8242f729dffe61d702471d56a6
Tropical Drink <emailfalsonumero4@gmail.com>
2016-05-25 04:51:42 +0100

    É melhor fazer commit para não perder nada

--- Eclipse Project/src/logic/HeuristicsUtils.java
+++ Eclipse Project/src/logic/HeuristicsUtils.java
@@ -1,39 +1,100 @@
 package logic;
 
+import logic.AStar.RestrictionType;
+
 public class HeuristicsUtils {
 	
-	// implementation of aux heuristics methods	
+	
+	/** 
+	 * Set this value to the graph start node
+	 */
+	public static Node startNode;
+	
+	/** 
+	 * Values for best cost per distance travelled and duration per distance unit travelled FOUND SO FAR.
+	 * Set them any time the A* expands the search list, including from the startNode.
+	 */
+	public static Double bestCostRatio, bestDurationRatio;
 
-	public static int UnrestrictedHeuristic(Node currentN, Node targetN){
-		Double h = 0.0;
-		h += DistanceValue(currentN, targetN);
+	/**
+	 * Choose wether to use Refueling Station Search and Resting Place Search
+	 */
+	public static boolean useFuel = false, useRest = false;
+
+	/**
+	 * Parameters that change how large the impact the requirements will have on the basic search heuristic.
+	 * Require fine-tuning on a per-case basis. Too low and they'll be ignored, too high and they'll take over the search for a solution. 
+	 */
+	public static int DurationWeight = 1, CostWeight = 1, RefuelWeight = 1, RestWeight = 1;
+	
+	
+	// implementation of aux heuristics methods		
+	public static double Heuristic(Node currentN, Node targetN){
 		
+		return DistanceValue(currentN, targetN);
 		
-		return h.intValue();
 	}
 	
-	public static double DistanceValue(Node A, Node B){
+	public static double Heuristic(Node currentN, Node targetN, RestrictionType type){
+		Double h = Heuristic(currentN, targetN);
+				
+		if (type == RestrictionType.DURATION || type == RestrictionType.DURATION_COST)
+			if(bestDurationRatio != null)
+				h += DurationWeight * DurationValue(currentN, targetN);
+		
+		if (type == RestrictionType.COST || type == RestrictionType.DURATION_COST)
+			if(bestCostRatio != null)
+				h += CostWeight * CostValue(currentN, targetN);
+
+		return h;
+	}
+	
+	public static double Heuristic(Node currentN, Node targetN, RestrictionType type, double FuelSpent, double RestSpent){
+		Double h = Heuristic(currentN, targetN, type);
+		
+		if(useFuel)
+			h += RefuelWeight * RefuelValue(FuelSpent);
+		
+		if(useRest)
+			h += RestWeight * RefuelValue(RestSpent);
+		
+		return h;
+	}
+	
+	public static double DistanceValue(Node currentN, Node targetN){
 		//Equals its Euclidean Distance
+		return EuclideanDistance(currentN, targetN);
+	}
+	
+	private static double CostValue(Node A, Node B){
+		
+		return 0;	
+	}
+	
+	
+	private static double DurationValue(Node currentN, Node targetN){
+		
+		return 0;
+	}
+	
+	
+	
+	/**
+	 * Should be used on both Fuel and Resting needs. Returns practically 0 for any input below 0.60.
+	 * Look at image "Desejo de Reabastecer" stored in the repository for a visual idea of what it does.
+	 * 
+	 * @param spentFuel should be a percentage based on maximum fuel
+	 * @return Value ranges from 0 and -1.
+	 */
+	private static double RefuelValue(double spentFuel){
+		double value = (spentFuel - 0.2);
+		value = -4.77 * Math.exp(-1/(value*value));
+		return value;
+	}
+	
+	public static double EuclideanDistance(Node A, Node B){
 		double dx = Math.abs(B.getXCord() - A.getXCord());
 		double dy = Math.abs(B.getYCord() - A.getYCord());
 		return Math.sqrt(dx * dx + dy * dy);
 	}
-	
-	public static double CostValue(Node A, Node B){
-		return 0;	
-	}
-	
-	public static double DurationValue(Node A, Node B){
-		return 0;
-	}
-	
-	public static double DrowzinessValue(Node A, Node B){
-		return 0;
-	}
-	
-	public static double FuelValue(Node A, Node B){
-		return 0;
-	}
-	
-	
 }
